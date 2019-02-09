b523e100a3b560849cc2f000930cc4267373f9dc
Zoe Bai <s.xue.bai@gmail.com>
2019-02-09 05:09:50 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  array1 = []
+  array.each do|num|
+    array1.push (num *= num)
+  end
+  return array1
 end
+
+# def square_array(array)
+#   return array.collect {|element| element *= element}
+# end
