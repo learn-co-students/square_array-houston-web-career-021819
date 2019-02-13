8108c3716707646c71a4efd6865c6211df3c77c1
Eli Lauffenburger <eli.lauffenburger@gmail.com>
2019-02-13 18:04:52 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
   # your code here
+  squared_array = []
+  array.each do |number|
+    square = number*number
+    squared_array.push(square)
+  end
+  return squared_array
 end
